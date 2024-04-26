3676f50eaeb4587d1db3e7baf8364466df536e72
ThulyoCM <thulyo.vytor@dcx.ufpb.br>
2024-04-26 00:20:59 -0300

    Engenaria

--- src/engenharia20232/Main.java
+++ src/engenharia20232/Main.java
@@ -3,7 +3,19 @@ package engenharia20232;
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		Thulio tv = new Thulio();
+		
+		Integer valor = tv.somar(4, 7);
+		
+		System.out.println(valor);
+		
+		
+		
+		
 		System.out.println("oi");
 	}
+	
+
 
 }
---
+++ src/engenharia20232/Thulio.java
@@ -0,0 +1,41 @@
+package engenharia20232;
+
+public class Thulio {
+	
+	private Integer a;
+	private Integer b;
+	private Integer total;
+	
+	public Integer somar(Integer a, Integer b) {
+		this.a = a;
+		this.b = b;
+		
+		this.total = getA() + getB();
+		
+		return getTotal();
+	}
+
+	public Integer getA() {
+		return a;
+	}
+
+	public void setA(Integer a) {
+		this.a = a;
+	}
+
+	public Integer getB() {
+		return b;
+	}
+
+	public void setB(Integer b) {
+		this.b = b;
+	}
+	
+	public Integer getTotal() {
+		return total;
+	}
+	
+	
+	
+
+}
